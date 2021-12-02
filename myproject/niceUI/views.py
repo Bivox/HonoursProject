98a5dd941429a2840414b71b33e0b5c69ecedba1
Mar-KO <mnaje006@uottawa.ca>
2021-12-02 10:38:07 -0500

    adding keras

--- myproject/niceUI/views.py
+++ myproject/niceUI/views.py
@@ -8,6 +8,8 @@ import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense,Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 import cv2
+import base64
+from flask import Flask, render_template, request, jsonify
 
 # Create your views here.
 def index(request):
@@ -20,6 +22,44 @@ def index(request):
 
    dests = [dest1, dest2]
    return render(request, "index.html", {'dests': dests})
+
+# Initialize flask app
+app = Flask(__name__)
+
+# Load prebuilt model
+model = keras.models.load_model('app/mnist_classification.h5')
+
+# Handle GET request
+@app.route('/', methods=['GET'])
+def drawing():
+    return render_template('drawing.html')
+
+# Handle POST request
+@app.route('/', methods=['POST'])
+def canvas():
+    # Recieve base64 data from the user form
+    canvasdata = request.form['canvasimg']
+    encoded_data = request.form['canvasimg'].split(',')[1]
+
+    # Decode base64 image to python array
+    nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    # Convert 3 channel image (RGB) to 1 channel image (GRAY)
+    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Resize to (28, 28)
+    gray_image = cv2.resize(gray_image, (28, 28), interpolation=cv2.INTER_LINEAR)
+
+    # Expand to numpy array dimenstion to (1, 28, 28)
+    img = np.expand_dims(gray_image, axis=0)
+
+    try:
+        prediction = np.argmax(model.predict(img))
+        print(f"Prediction Result : {str(prediction)}")
+        return render_template('drawing.html', response=str(prediction), canvasdata=canvasdata, success=True)
+    except Exception as e:
+        return render_template('drawing.html', response=str(e), canvasdata=canvasdata)
 
 def digit_rec_model(request):
    #hand written characters 28x28 sized images of 0...9
