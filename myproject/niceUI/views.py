46961983143fca3e3b68643954c2263a9ad08332
Mar-KO <mnaje006@uottawa.ca>
2021-12-03 11:30:27 -0500

    fixed digit_rec.h5

--- myproject/niceUI/views.py
+++ myproject/niceUI/views.py
@@ -129,7 +129,7 @@ def digit_rec_model(request):
     predictions=model.predict([x_testr])
 
     # Save model
-    model.save('digit_rec.h5')
+    model.save('niceUI/digit_rec.h5')
 
     # Evaluating the predictions
     # Comparing test data vs predicted data
