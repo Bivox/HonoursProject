a097980d1602892235d5b31c7dca0c7c44688a5d
Mar-KO <mnaje006@uottawa.ca>
2021-12-15 22:27:21 -0500

    done!

--- myproject/niceUI/views.py
+++ myproject/niceUI/views.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense,Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Dense,Dropout, Activation, Flatten, Conv2D, MaxPooling2D, LeakyReLU, BatchNormalization
 import cv2
 import base64
 from flask import Flask, render_template, request, jsonify
@@ -37,12 +37,13 @@ def drawing():
 @app.route('/', methods=['POST'])
 def canvas(request):
     # Recieve base64 data from the user form
-    canvasdata = request.form["canvasimg"]
-    encoded_data = request.form['canvasimg'].split(',')[1]
+    #canvasdata = request.form["canvasimg"]
+    #encoded_data = request.form['canvasimg'].split(',')[1]
 
     # Decode base64 image to python array
-    nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    #nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
+    #img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    img = cv2.imread('/Users/Marco/Downloads/hand_written_digit.png')
 
     # Convert 3 channel image (RGB) to 1 channel image (GRAY)
     gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -56,10 +57,10 @@ def canvas(request):
     try:
         prediction = np.argmax(reconstructed_model.predict(img))
         print(f"Prediction Result : {str(prediction)}")
-        return render_template('index.html', response=str(prediction), canvasdata=canvasdata, success=True)
+        return render('index.html', {'prediction':prediction})
     except Exception as e:
 
-        return render_template('index.html', response=str(e), canvasdata=canvasdata)
+        return render('index.html', {'prediction':prediction})
 
 def load_model(request):
     # Load prebuilt model
@@ -86,13 +87,58 @@ def digit_rec_model():
     #print("Training Samples dimension",x_trainr.shape)
     #print("Testing Samples dimension",x_testr.shape)
 
-    # Creating a DNN (Deep Neural Network)
-    # Training with 60,000 samples
-    # create a neural net
     model=Sequential()
 
     # first convolution layer  (60000,28,28,1) 28-3+1=26x26
-    model.add(Conv2D(64,(3,3), input_shape=x_trainr.shape[1:])) # 64 filters with size of 3x3
+    model.add(Conv2D(64,(3,3), input_shape=(28, 28, 1))) # 64 filters with size of 3x3
+    model.add(LeakyReLU()) # activation function to make it non-linear, <0, remove, >0
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2,2))) #maxpooling single maximum value of 2x2
+
+    # 2nd convolution layer    26-3+1=24x24
+    model.add(Conv2D(64,(3,3)))
+    model.add(LeakyReLU())
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2,2)))
+
+    # 3rd convolution layer
+    model.add(Conv2D(64,(3,3)))  #13x13
+    model.add(LeakyReLU())
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2,2)))
+
+    # Fully connected layer #1   20x20=400
+    model.add(Flatten()) #before using fully connected layer, need to be flatten so that 2D to 1D
+
+
+    # Fully connected layer #2   20x20=400 
+  
+
+    # Fully connected layer #3 (LAST)   20x20=400
+    model.add(Dense(10))    # the last dense layer must be equal to 10
+    model.add(Activation("softmax")) #activation with Softmax (can also be sigmoid for BINARY classification)(class probabilities, not for binary)
+    # softmax is useful for probability distributions
+
+    model.compile(loss="sparse_categorical_crossentropy", optimizer="adam",metrics=['accuracy'])
+
+    # Training the model
+    model.fit(x_trainr, y_train, epochs=5,validation_split=0.1)
+
+    # Predictions
+    # preditions are an array of class probabilities, so we need to decode them
+    predictions=model.predict([x_testr])
+
+    
+    # Save model
+    model.save('niceUI/digit_rec.h5')
+
+    # Creating a DNN (Deep Neural Network)
+    # Training with 60,000 samples
+    # create a neural net
+    '''model=Sequential()
+
+    # first convolution layer  (60000,28,28,1) 28-3+1=26x26
+    model.add(Conv2D(64,(3,3), input_shape=(28, 28, 1))) # 64 filters with size of 3x3
     model.add(Activation("relu")) # activation function to make it non-linear, <0, remove, >0
     model.add(MaxPooling2D(pool_size=(2,2))) #maxpooling single maximum value of 2x2
 
@@ -123,14 +169,14 @@ def digit_rec_model():
     model.compile(loss="sparse_categorical_crossentropy", optimizer="adam",metrics=['accuracy'])
 
     # Training the model
-    model.fit(x_trainr, y_train, epochs=3,validation_split=0.3)
+    model.fit(x_trainr, y_train, epochs=5,validation_split=0.1)
 
     # Predictions
     # preditions are an array of class probabilities, so we need to decode them
     predictions=model.predict([x_testr])
-
+    
     # Save model
-    model.save('niceUI/digit_rec.h5')
+    model.save('niceUI/digit_rec.h5')'''
 
     # Evaluating the predictions
     # Comparing test data vs predicted data
@@ -153,31 +199,48 @@ def crop(img, x = 0, y = 0, w = 800, h = 800):
 
 def predict_digit(request):
     # Load prebuilt model
-    #reconstructed_model = tf.keras.models.load_model('niceUI/digit_rec.h5')
-    reconstructed_model = digit_rec_model()
+    reconstructed_model = tf.keras.models.load_model('niceUI/digit_rec.h5')
+    #reconstructed_model = digit_rec_model()
     IMG_SIZE=28
     img = cv2.imread('/Users/Marco/Downloads/hand_written_digit.png')
-    print("img shape:",img.shape)
+    #print("img shape:",img.shape)
     #TODO img_left, img_right = crop(img)
     imgray_1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     #ret, thresh = cv2.threshold(imgray_1, 127, 255, 0)
     thresh = cv2.Canny(imgray_1, 100, 100 * 2)
     #canny_output=cv2.blur(canny_output, (5,5))
-    contours = cv2.findContours(imgray_1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    contours = contours[0] if len(contours) == 2 else contours[1]
+    contours = []
+    contours_tmp = cv2.findContours(imgray_1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours_tmp = contours_tmp[0] if len(contours_tmp) == 2 else contours_tmp[1]
+    imgray_1_copy = imgray_1.copy()
+    while 0 != len(contours_tmp):
+        contours.append(contours_tmp[0])
+        x, y, w, h = cv2.boundingRect(contours_tmp[0])
+        imgray_1_copy = cv2.rectangle(imgray_1_copy, (x,y), (x+w,y+h), (0,0,0), -1)
+        contours_tmp = cv2.findContours(imgray_1_copy, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours_tmp = contours_tmp[0] if len(contours_tmp) == 2 else contours_tmp[1]
+        pass
     #creating a list of cropped imgs
     lst = []
+    coord_lst = []
     for c in contours:
         x, y, w, h = cv2.boundingRect(c)
+        coord_lst.append(x)
         #print(x, y, w, h)
-        
+        canvas_width = w + 50 if w + 50 > 300 else 300
+        canvas_height = h + 50
+        x_center = (canvas_width - w) // 2
+        y_center = (canvas_height - h) // 2
         cropped = crop(imgray_1, x, y, w, h)
+        print(x, y)
         print("--------- cropped dtype",cropped.shape)
-        result_image = np.full((800,800,1),0, dtype=np.uint8)
+
+        result_image = np.full((canvas_height,canvas_width,1),0, dtype=np.uint8)
         print("1----result_image shape:",result_image.shape)
         # copy img image into center of result image
         cropped = np.expand_dims(cropped, axis=-1)
-        result_image[400:400 + h, 400:400+w] = cropped
+        result_image[y_center:y_center+h, x_center:x_center+w] = cropped
+        #result_image[0:0 + h, 0:0+w] = cropped
 
         cv2.imshow("dying inside", result_image)
         cv2.waitKey(0)
@@ -190,12 +253,15 @@ def predict_digit(request):
     else:
         print ("Colored image")'''
     result=""
+
+    res = dict(zip(coord_lst, lst))
+    lst=dict(sorted(res.items(),key= lambda x:x[0])).values()
     for c in lst:
-        cv2.imshow("dying inside", c)
+        
+        resized=cv2.resize(c, (IMG_SIZE,IMG_SIZE), interpolation=cv2.INTER_AREA)
+        cv2.imshow("dying inside", resized)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-        resized=cv2.resize(c, (IMG_SIZE,IMG_SIZE), interpolation=cv2.INTER_AREA)
-    
         # 0 to 1 scaling
         norm_img=tf.keras.utils.normalize(resized,axis=1) 
         #kernel operation of convolution layer
